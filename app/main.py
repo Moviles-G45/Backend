43569ce65b302bf2ca1fdc0990078694ef36723d
Julian Escobar <escobarjulian347@gmail.com>
2025-03-16 22:41:09 -0500

    Add initial FastAPI setup with database configuration and environment management

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from tortoise import exceptions as db_exception
+from tortoise.contrib.fastapi import register_tortoise
+# from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.settings import TORTOISE_ORM, env
+
+app = FastAPI(title=env.APP_NAME, version=env.APP_VERSION)
+
+
+# origins = [
+
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+try:
+    register_tortoise(
+        app,
+        config=TORTOISE_ORM,
+        add_exception_handlers=True,
+        generate_schemas=True
+    )
+except db_exception.ConfigurationError as e:
+    print(f"An error has ocurred while configuring the database: {e}")
+    raise e
+except db_exception.DBConnectionError as e:
+    print(f"An error has ocurred while connecting to the database: {e}")
+    raise e
+
+@app.get("/ok", tags=["health"])
+async def health_check():
+    return {"status": "ok"}
