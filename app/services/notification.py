716afcabe351537ac1d854b97d4318f4ba703a67
Juan-David-Obando-Novoa <j.obandon@uniandes.edu.co>
2025-03-17 16:31:43 -0500

    Categoria y Notificacion

--- app/services/notification.py
+++ app/services/notification.py
@@ -1,7 +1,13 @@
+from app.models.notification import Notification
+from app.schemas.notification import NotificationCreate, NotificationPydantic, NotificationPydanticIn
+
+
+
 async def get_notifications():
     """Fetch user notifications from DB."""
-    pass
+    return await NotificationPydantic.from_queryset(Notification.all())
 
-async def create_notification():
+async def create_notification(notification: NotificationCreate):
     """Insert a new notification into DB."""
-    pass
+    notification_obj = await Notification.create(**notification.model_dump(exclude_unset=True))
+    return await NotificationPydantic.from_tortoise_orm(notification_obj)
