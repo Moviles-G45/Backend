9165fe29055cc95c3aac28830e9e4824d38ca6ed
Julian Escobar <escobarjulian347@gmail.com>
2025-03-18 16:25:12 -0500

    Complete getCurrentUser

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends
 import httpx
 from ..services.auth import signup, login, recover_password
-from ..schemas.user import UserRequest, UserLogin
+from ..schemas.user import UserRequest, UserLogin, UserClassicLogin
 from ..auth.dependencies import get_current_user
 
 
@@ -30,34 +30,34 @@ def recover_password_route(email: str):
         return recover_password(email)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
+
 @router.get("/me")
 def getCurrentUser(user = Depends(get_current_user)):
 
     return user
 
 
-@router.post("/get-token")
-async def get_token(user: UserLogin):
+@router.get("/token")
+async def get_token(user: UserClassicLogin):
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBAviu-38dW9SF4U9UmVORslfWnr5hI_uQ"
     payload = {
         "email": user.email,
         "password": user.password,
         "returnSecureToken": True
     }
-    
+
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json=payload)
-    
+
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=f"Error en login: {response.text}")
-    
+
     data = response.json()
     id_token = data.get("idToken")
-    
+
     if not id_token:
         raise HTTPException(status_code=400, detail="No se pudo obtener el idToken")
-    
+
     return {"idToken": id_token}
 
 
